7052e62b942a01d039054a24103220ca7087eefe
Punit Raj <raj.punit3299@gmail.com>
2021-11-27 16:37:16 +0530

    first commit

---
+++ src/main/java/com/sanyukt/SanyuktAppApplication.java
@@ -0,0 +1,13 @@
+package com.sanyukt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SanyuktAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SanyuktAppApplication.class, args);
+	}
+
+}
